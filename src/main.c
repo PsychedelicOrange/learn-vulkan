745ac7e5f57ac3aefa24db65364e41e4a9610f47
psyomen <parthsaravade.acad@gmail.com>
2025-01-28 13:11:58 +0530

    create instance and print extensions

--- src/main.c
+++ src/main.c
@@ -1,20 +1,14 @@
+#include <stdint.h>
+#include <vulkan/vulkan_core.h>
 #define GLFW_INCLUDE_VULKAN
 #include "stdio.h"
 #include <GLFW/glfw3.h>
 
 #include "clib/log.h"
 
-void init_glfw(int gl_major, int gl_minor) {
+void init_glfw() {
   glfwInit();
-  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, gl_major);
-  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, gl_minor);
-  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-
-#ifdef __APPLE__
-  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
-  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
-  glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
-#endif
+  glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
 }
 
 void *create_glfw_window(int SCR_WIDTH, int SCR_HEIGHT,
@@ -29,23 +23,56 @@ void *create_glfw_window(int SCR_WIDTH, int SCR_HEIGHT,
   return window;
 }
 
-int main() {
-  glfwInit();
-
-  glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
-  GLFWwindow *window = glfwCreateWindow(800, 600, "Vulkan window", 0, 0);
+void createInstance() {
 
   uint32_t extensionCount = 0;
   vkEnumerateInstanceExtensionProperties(0, &extensionCount, 0);
+  logi(" %i extensions supported", extensionCount);
+  VkExtensionProperties extensionProperties[extensionCount];
+  vkEnumerateInstanceExtensionProperties(0, &extensionCount,
+                                         extensionProperties);
+  for (uint32_t i = 0; i < extensionCount; i++) {
+    logi("%s,", extensionProperties[i].extensionName);
+  }
 
-  logp(" %i extensions supported\n", extensionCount);
+  VkApplicationInfo applicationInfo = {
+      .sType = VK_STRUCTURE_TYPE_APPLICATION_INFO,
+      .pApplicationName = "learn-vulkan",
+      .applicationVersion = VK_MAKE_VERSION(1, 0, 0),
+      .pEngineName = "none",
+      .engineVersion = VK_MAKE_VERSION(1, 0, 0),
+      .apiVersion = VK_MAKE_VERSION(1, 4, 304)};
+
+  uint32_t glfwExtensionCount = 0;
+  const char **glfwExtensions;
+
+  glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+
+  VkInstanceCreateInfo createInfo = {
+      .sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO,
+      .pApplicationInfo = &applicationInfo,
+      .enabledLayerCount = 0,
+      .enabledExtensionCount = glfwExtensionCount,
+      .ppEnabledExtensionNames = glfwExtensions};
+
+  VkInstance instance;
+  VkResult result;
+  if ((result = vkCreateInstance(&createInfo, NULL, &instance)) != VK_SUCCESS) {
+    loge("VkResult[%i]: Couldn't create vulkan instance!", result);
+  }
+}
+
+int main() {
+  init_glfw();
+  GLFWwindow *window = create_glfw_window(800, 600, "Vulkan window");
+
+  createInstance();
 
   while (!glfwWindowShouldClose(window)) {
     glfwPollEvents();
   }
 
   glfwDestroyWindow(window);
-
   glfwTerminate();
 
   return 0;
